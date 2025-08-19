448595fc32d025be8ec2aa6c48791244981e218b
Carmen Oliveira <carmennishi@gmail.com>
2025-08-18 21:21:38 -0300

    Adding files with the implementation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Cadastrando um Smartphone da Nokia");
+
+Smartphone nokia = new Nokia("11999013256", "Nokia G21", "31239855019123", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Cadastrando um Smartphone Iphone");
+
+Smartphone iphone = new Iphone("11985315507", "Iphone 16 Pro", "901235706134605", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
